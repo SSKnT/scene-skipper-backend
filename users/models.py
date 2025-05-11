49ebb8cd7cb3452edda366583c78335440a9dd13
SSKnT <sohaibshaukat78@gmail.com>
2025-05-11 20:45:24 +0500

    Refactor CustomUserSerializer to clarify read-only fields

--- users/models.py
+++ users/models.py
@@ -5,4 +5,4 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'role']
-        read_only_fields = ['id', 'is_staff', 'is_active', 'role']  # optional: make 'role' read-only if only admin sets it
+        read_only_fields = ['id', 'is_staff', 'is_active', 'role']
